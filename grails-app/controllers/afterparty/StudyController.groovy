93077c6339ded8652da92aa290075b3c48a6d2d0
martin <martin@martin-S5000XVN.(none)>
2011-12-14 13:00:27 +0000

    removed delete action from study controller as it's unlikely to work anyway

--- grails-app/controllers/afterparty/StudyController.groovy
+++ grails-app/controllers/afterparty/StudyController.groovy
@@ -81,22 +81,5 @@ class StudyController {
     }
 
 
-    def delete = {
-        def studyInstance = Study.get(params.id)
-        if (studyInstance) {
-            try {
-                studyInstance.delete(flush: true)
-                flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'study.label', default: 'Study'), params.id])}"
-                redirect(action: "list")
-            }
-            catch (org.springframework.dao.DataIntegrityViolationException e) {
-                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'study.label', default: 'Study'), params.id])}"
-                redirect(action: "show", id: params.id)
-            }
-        }
-        else {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'study.label', default: 'Study'), params.id])}"
-            redirect(action: "list")
-        }
-    }
+
 }
