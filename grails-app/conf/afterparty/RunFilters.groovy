119074647af262b58a6a7f713f1435c24290c4d3
martin <martin@martin-S5000XVN.(none)>
2011-12-16 12:50:07 +0000

    added filters to run controller

---
+++ grails-app/conf/afterparty/RunFilters.groovy
@@ -0,0 +1,67 @@
+package afterparty
+
+class RunFilters {
+
+    def springSecurityService
+
+    def filters = {
+
+        uploadedFileExists(controller: 'run', action: '(attachRawReads|attachTrimmedReads)') {
+            before = {
+                println "running uploaded file filter"
+                def f = request.getFile('myFile')
+
+                if (f.empty) {
+                    flash.error = "File cannot be empty"
+                    redirect(controller: 'run', action: 'show', id: params.id)
+                    return false
+                }
+            }
+
+        }
+
+        runExists(controller: 'run', action: '(attachRawReads|attachTrimmedReads|show)') {
+            before = {
+                println "checking if run exists"
+                Run a = Run.get(params.id)
+                if (!a) {
+                    flash.error = "Run doesn't exist"
+                    redirect(controller: 'study', action:'listPublished')
+                    return false
+                }
+            }
+        }
+
+        runIsPublicOrOwnedByUser(controller: 'run', action: '(show)') {
+            before = {
+                println "checking if run is either public or owned"
+                Run r = Run.get(params.id)
+                def user = springSecurityService.isLoggedIn() ? springSecurityService?.principal : null
+
+                if (!r.experiment.sample.study.published && r.experiment.sample.study.user.id != user?.id) {
+                    flash.error = "Run is not published and you are not the owner"
+                    redirect(controller: 'study', action:'listPublished')
+                    return false
+                }
+            }
+        }
+
+
+        runIsOwnedByUser(controller: 'run', action: '(attachTrimmedReads|attachRawReads)') {
+            before = {
+                println "checking if run is owned by user"
+                Run a = Run.get(params.id)
+                if (r.experiment.sample.study.user.id != springSecurityService.principal.id) {
+                    flash.error = "Run doesn't belong to you"
+                    redirect(controller: 'run', action: 'show', id: params.id)
+                    return false
+                }
+            }
+        }
+
+
+
+
+    }
+
+}
--- grails-app/controllers/afterparty/RunController.groovy
+++ grails-app/controllers/afterparty/RunController.groovy
@@ -1,93 +1,90 @@
 package afterparty
 
+import grails.plugins.springsecurity.Secured
+
 class RunController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
+    @Secured(['ROLE_USER'])
     def create = {
-        def runInstance = new Run(name : 'Run name', description: 'Run description')
+        def runInstance = new Run(name: 'Run name', description: 'Run description')
         Experiment.get(params.experimentId.toLong()).addToRuns(runInstance)
         runInstance.save()
-        redirect(action: show, id : runInstance.id)
+        redirect(action: show, id: runInstance.id)
     }
 
 
 
+    @Secured(['ROLE_USER'])
     def attachRawReads = {
         def f = request.getFile('myFile')
-        if (!f.empty) {
-            def runId = params.runId
-            BackgroundJob job = new BackgroundJob(
-                    name: 'adding FASTQ file',
-                    status: BackgroundJobStatus.QUEUED,
-                    progress: 'queued',
-                    type: BackgroundJobType.UPLOAD_READS,
-                    study: Run.get(runId).experiment.sample.study
-            )
+
+        def runId = params.runId
+        BackgroundJob job = new BackgroundJob(
+                name: 'adding FASTQ file',
+                status: BackgroundJobStatus.QUEUED,
+                progress: 'queued',
+                type: BackgroundJobType.UPLOAD_READS,
+                study: Run.get(runId).experiment.sample.study
+        )
+        job.save(flush: true)
+        runAsync {
+
+            Run run = Run.get(runId)
+
+
+            println "attaching FASTQ file to run ${run}"
+            job.status = BackgroundJobStatus.RUNNING
+            job.progress = "attaching FASTQ file to run ${run}"
             job.save(flush: true)
-            runAsync {
+            ReadsFileData d = new ReadsFileData(fileData: f)
+            ReadsFile r = new ReadsFile(name: "uploaded FASTQ ${f.originalFilename} for ${run.name}", data: d, status: ReadsFileStatus.RAW)
+            run.addToReadsFiles(r)
+            run.save()
+            job.status = BackgroundJobStatus.FINISHED
 
-                Run run = Run.get(runId)
-
-
-                println "attaching FASTQ file to run ${run}"
-                job.status = BackgroundJobStatus.RUNNING
-                job.progress = "attaching FASTQ file to run ${run}"
-                job.save(flush: true)
-                ReadsFileData d = new ReadsFileData(fileData: f)
-                ReadsFile r = new ReadsFile(name: "uploaded FASTQ ${f.originalFilename} for ${run.name}", data: d, status: ReadsFileStatus.RAW)
-                run.addToReadsFiles(r)
-                run.save()
-                job.status = BackgroundJobStatus.FINISHED
-
-                job.save(flush: true)
-            }
-
-            redirect(controller: 'backgroundJob', action: list)
+            job.save(flush: true)
         }
-        else {
-            flash.message = 'file cannot be empty'
-            render(view: 'uploadForm')
-        }
+
+        redirect(controller: 'backgroundJob', action: list)
+
     }
 
 
+    @Secured(['ROLE_USER'])
     def attachTrimmedReads = {
         def f = request.getFile('myFile')
-        if (!f.empty) {
-            def runId = params.runId
-            BackgroundJob job = new BackgroundJob(
-                    name: 'adding trimmed reads file',
-                    status: BackgroundJobStatus.QUEUED,
-                    progress: 'queued',
-                    type: BackgroundJobType.UPLOAD_READS,
-                    study: Run.get(runId).experiment.sample.study
-            )
+
+        def runId = params.runId
+        BackgroundJob job = new BackgroundJob(
+                name: 'adding trimmed reads file',
+                status: BackgroundJobStatus.QUEUED,
+                progress: 'queued',
+                type: BackgroundJobType.UPLOAD_READS,
+                study: Run.get(runId).experiment.sample.study
+        )
+        job.save(flush: true)
+        runAsync {
+
+            Run run = Run.get(runId)
+
+
+            println "attaching FASTQ file to run ${run}"
+            job.status = BackgroundJobStatus.RUNNING
+            job.progress = "attaching FASTQ file to run ${run}"
             job.save(flush: true)
-            runAsync {
+            ReadsFileData d = new ReadsFileData(fileData: f)
+            ReadsFile r = new ReadsFile(name: "uploaded FASTQ ${f.originalFilename} for ${run.name}", data: d, status: ReadsFileStatus.TRIMMED)
+            run.trimmedReadsFile = r
+            run.save()
+            job.status = BackgroundJobStatus.FINISHED
 
-                Run run = Run.get(runId)
-
-
-                println "attaching FASTQ file to run ${run}"
-                job.status = BackgroundJobStatus.RUNNING
-                job.progress = "attaching FASTQ file to run ${run}"
-                job.save(flush: true)
-                ReadsFileData d = new ReadsFileData(fileData: f)
-                ReadsFile r = new ReadsFile(name: "uploaded FASTQ ${f.originalFilename} for ${run.name}", data: d, status: ReadsFileStatus.TRIMMED)
-                run.trimmedReadsFile = r
-                run.save()
-                job.status = BackgroundJobStatus.FINISHED
-
-                job.save(flush: true)
-            }
-
-            redirect(controller: 'backgroundJob', action: list)
+            job.save(flush: true)
         }
-        else {
-            flash.message = 'file cannot be empty'
-            render(view: 'uploadForm')
-        }
+
+        redirect(controller: 'backgroundJob', action: list)
+
     }
 
     def show = {
@@ -100,10 +97,6 @@ class RunController {
             [runInstance: runInstance]
         }
     }
-
-
-
-
 
 
 }
